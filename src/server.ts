5650893671fb415831647def5f7681bdf07e8fc0
Fahmida Hamid <fahmida.hamid@gmail.com>
2026-05-23 00:48:42 -0400

    await initializeDatabase()

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import config from "./config";
 import { initializeDatabase } from "./db";
 
 const main = async () => {
-  initializeDatabase();
+  await initializeDatabase();
   app.listen(config.port, () => {
     console.log(`Server is running at port: ${config.port}`);
   });
